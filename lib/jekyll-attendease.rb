6f431b0998eb449721a809a2be7b0bb53736f119
Jamie Lubiner <jamie@coverallcrew.com>
2013-08-22 16:57:31 -0700

    Namespaced and added accessor method.

--- lib/jekyll-attendease.rb
+++ lib/jekyll-attendease.rb
@@ -228,39 +228,46 @@ layout: layout
 
         content = <<-eos
 <script type="text/javascript">
-var attendease_logged_in = false;
 
-function handleAuthState()
-{
-  var xmlhttp;
-  if (window.XMLHttpRequest)
-  {
-    xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function()
+var JekyllAttendease = {
+
+  attendease_logged_in : false,
+
+  isLoggedIn: function() {
+    return this.attendease_logged_in;
+  },
+
+  handleAuthState: function() {
+    var xmlhttp;
+    if (window.XMLHttpRequest)
     {
-      if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      xmlhttp=new XMLHttpRequest();
+      xmlhttp.onreadystatechange=function()
       {
-        logout = '<a class="attendease-auth-logout" href="/attendease/logout">Logout</a>';
-        document.getElementById("attendease-auth-action").innerHTML = logout;
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+          logout = '<a class="attendease-auth-logout" href="/attendease/logout">Logout</a>';
+          document.getElementById("attendease-auth-action").innerHTML = logout;
 
-        account = JSON.parse(xmlhttp.responseText);
-        account = '<a class="attendease-auth-account" href="/attendease/account">' + account.name + '</a>';
-        document.getElementById("attendease-auth-account").innerHTML = account;
+          account = JSON.parse(xmlhttp.responseText);
+          account = '<a class="attendease-auth-account" href="/attendease/account">' + account.name + '</a>';
+          document.getElementById("attendease-auth-account").innerHTML = account;
 
-        attendease_logged_in = true;
+          this.attendease_logged_in = true;
+        }
+        else
+        {
+          login = '<a class="attendease-auth-logout" href="/attendease/login">Login</a>';
+          document.getElementById("attendease-auth-action").innerHTML = login;
+        }
       }
-      else
-      {
-        login = '<a class="attendease-auth-logout" href="/attendease/login">Login</a>';
-        document.getElementById("attendease-auth-action").innerHTML = login;
-      }
+      xmlhttp.open("GET","/attendease/verify_credentials.json",true);
+      xmlhttp.send();
     }
-    xmlhttp.open("GET","/attendease/verify_credentials.json",true);
-    xmlhttp.send();
   }
-}
+};
 
-document.addEventListener('DOMContentLoaded', handleAuthState);
+document.addEventListener('DOMContentLoaded', JekyllAttendease.handleAuthState);
 </script>
         eos
 
