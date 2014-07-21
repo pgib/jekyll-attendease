0e631999114c48ac7786bc33078e8575dde11add
Michael Wood <mike@coverallcrew.com>
2014-07-21 16:07:06 -0700

    Update specs to point to the `_attendease_layouts` folder for page generation layouts.

--- spec/lib/jekyll/attendease_plugin/event_layout_generator_spec.rb
+++ spec/lib/jekyll/attendease_plugin/event_layout_generator_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Jekyll::AttendeasePlugin::EventLayoutGenerator do
 
   it 'creates the layout stubs' do
     %w{ layout register schedule presenters venues sponsors }.each do |layout|
-      expect(File.exists?(File.join(@site.source, 'attendease_layouts', "#{layout}.html"))).to eq(true)
+      expect(File.exists?(File.join(@site.source, '_attendease_layouts', "#{layout}.html"))).to eq(true)
     end
   end
 
