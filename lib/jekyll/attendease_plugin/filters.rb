7812406ff68a965870f0980dcfe80c7cd6fa8ed9
Antoine Censi <antoine.censi@gmail.com>
2016-02-18 16:19:35 -0800

    Revert "Add safe_json for javascript json string output [#75]"
    This reverts commit dc24d909443d125d7ad8ffc9e79fe26719a6f22e.

--- lib/jekyll/attendease_plugin/filters.rb
+++ lib/jekyll/attendease_plugin/filters.rb
@@ -4,19 +4,8 @@ module Jekyll
       def slugify(string)
         Helpers.parameterize(string, '_')
       end
-
       def json(obj)
         obj.to_json
-      end
-
-      def safe_json(obj)
-        obj = obj.to_json
-        obj.gsub!(/[^\x00-\x7F]/,'')             # escape non-ascii
-        obj.gsub!(/\\[nrt]/,'')                  # escape multiline
-        obj.gsub!('\"','escaped-quotes')         # escape content quotes I
-        obj.gsub!('"','\"')                      # escape quoting quotes
-        obj.gsub!('escaped-quotes','\\\\\\\\\"') # escape content quotes II
-        obj
       end
     end
   end
