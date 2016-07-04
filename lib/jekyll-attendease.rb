cd2a6b7e18551334d4674de8e68c6a85e61aba87
Patrick Gibson <patrick@pgib.me>
2016-07-04 16:08:05 -0700

    Minor tidying up.

--- lib/jekyll-attendease.rb
+++ lib/jekyll-attendease.rb
@@ -6,12 +6,12 @@ require 'digest'
 
 require 'jekyll/attendease_plugin'
 
-Liquid::Template.register_tag('attendease_schedule_widget', Jekyll::AttendeasePlugin::ScheduleWidgetTag)
-Liquid::Template.register_tag('attendease_auth_script', Jekyll::AttendeasePlugin::AuthScriptTag)
+Liquid::Template.register_tag('attendease_schedule_widget',  Jekyll::AttendeasePlugin::ScheduleWidgetTag)
+Liquid::Template.register_tag('attendease_auth_script',      Jekyll::AttendeasePlugin::AuthScriptTag)
 Liquid::Template.register_tag('attendease_scheduler_script', Jekyll::AttendeasePlugin::SchedulerScriptTag)
-Liquid::Template.register_tag('attendease_locales_script', Jekyll::AttendeasePlugin::LocalesScriptTag)
-Liquid::Template.register_tag('attendease_auth_account', Jekyll::AttendeasePlugin::AuthAccountTag)
-Liquid::Template.register_tag('attendease_auth_action', Jekyll::AttendeasePlugin::AuthActionTag)
-Liquid::Template.register_tag('attendease_t', Jekyll::AttendeasePlugin::TranslateTag)
+Liquid::Template.register_tag('attendease_locales_script',   Jekyll::AttendeasePlugin::LocalesScriptTag)
+Liquid::Template.register_tag('attendease_auth_account',     Jekyll::AttendeasePlugin::AuthAccountTag)
+Liquid::Template.register_tag('attendease_auth_action',      Jekyll::AttendeasePlugin::AuthActionTag)
+Liquid::Template.register_tag('attendease_t',                Jekyll::AttendeasePlugin::TranslateTag)
 
 Liquid::Template.register_filter(Jekyll::AttendeasePlugin::Filters)
