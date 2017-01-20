6c5636048f534ca578c4fa277dee4238f55d3c17
Patrick Gibson <patrick@pgib.me>
2017-01-20 13:38:35 -0800

    Generate pre-rendered layouts for all CMS layouts.

--- lib/jekyll/attendease_plugin/event_layout_generator.rb
+++ lib/jekyll/attendease_plugin/event_layout_generator.rb
@@ -31,6 +31,20 @@ module Jekyll
           File.open(base_layout_file, 'w+') { |f| f.write("{{ content }}") }
         end
 
+        cms_layouts = []
+        begin
+          Dir.glob(File.join(layouts_path, '*.html')).each do |l|
+            html = File.read(l)
+            if html =~ %r!\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)!m
+              data = YAML.load(Regexp.last_match(1))
+              if data['cms_layout']
+                cms_layouts << File.basename(l).split('.').first
+              end
+            end
+          end
+        rescue
+        end
+
         layouts_to_precompile = %w{ layout register surveys } # These are compiled to the html site.
         layouts_to_precompile.each do |layout|
           # create a layout file if it already doesn't exist.
@@ -42,6 +56,9 @@ module Jekyll
           end
         end
 
+        cms_layouts.each do |layout|
+          site.pages << EventLayoutPage.new(site, site.source, 'attendease_layouts', "cms-#{layout}.html", layout, layout.capitalize)
+        end
 
         # Precompiled layouts for attendease email
         base_email_layout = site.config['attendease']['base_email_layout'] || 'email'
