d4b2e204a8d3e5afc204eae30827517ce5c18d0a
Canuckaholic <ajones@popdigital.ca>
2018-11-28 17:24:16 -0800

    Adding AnalyticsSettings tag for exposing Google Analytics ID

--- lib/jekyll/attendease_plugin/tags.rb
+++ lib/jekyll/attendease_plugin/tags.rb
@@ -326,8 +326,6 @@ _EOT
           || analytics['googleAnalyticsTrackingId'].nil? \
           || analytics['googleAnalyticsTrackingId'].empty?
 
-        return '' if analytics.nil? || !analytics['googleAnalyticsTrackingId']
-
         adwordsId = analytics['googleAnalyticsAdwordsId']
         script = <<_EOT
 <!-- Global Site Tag (gtag.js) - Google Analytics -->
@@ -379,7 +377,6 @@ _EOT
         site_settings = context.registers[:site].data['site_settings'].clone
         analytics = site_settings['analytics']
 
-        return '' if analytics.nil? || !analytics['facebookPixelId']
         return '' if analytics.nil? \
           || analytics['facebookPixelId'].nil? \
           || analytics['facebookPixelId'].empty?
@@ -401,6 +398,27 @@ _EOT
   src="https://www.facebook.com/tr?id=#{analytics['facebookPixelId']}&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
+_EOT
+        script
+      end
+    end
+
+    class AnalyticsSettingsTag < Liquid::Tag
+      def render(context)
+        site_settings = context.registers[:site].data['site_settings'].clone
+        analytics = site_settings['analytics']
+
+        return '' if analytics.nil? \
+          || analytics['googleAnalyticsTrackingId'].nil? \
+          || analytics['googleAnalyticsTrackingId'].empty?
+
+        script = <<_EOT
+<!-- Global Analytics Settings -->
+<script>
+  window.AnalyticsSettings = {
+    googleAnalyticsTrackingId: '#{analytics['googleAnalyticsTrackingId']}'
+  }
+</script>
 _EOT
         script
       end
